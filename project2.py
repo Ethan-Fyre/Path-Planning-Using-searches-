3c4d732a7b23107c87809269bf830e463615281a
Ethan-Fyre <jasayles@anderson.edu>
2018-02-13 23:45:38 -0500

    Actions, Results, Goal_Test implemented.

--- project2.py
+++ project2.py
@@ -4,6 +4,8 @@
 #
 # Purpose: Create a program that utilizes various searching algorithms to find the optimal solution through a terrain.
 
+import sys
+sys.path.insert(0, './aima-python-master')
 import argparse
 import numpy as np
 import search as aima
@@ -13,26 +15,44 @@ class Search(aima.Problem):
     def __init__(self, initial, goal, graph):
         aima.Problem.__init__(self, initial, goal)
         self.graph = graph
+        self.width = len(self.graph[0])
+        self.height = len(self.graph)
+
     def actions(self, state):
-        #State in this case is the coords of the current position
-        '''If edge piece only actions are away from edge
-            if state == [0,0]: actions are d, dr, r
-            elif state == [0,height]: actions are u, ur, r
-            elif state == [width, 0]: actions are l, dl, d
-            elif state == [width, height]: actions are u, ul, l
-            elif state[0] == 0: actions are u, ur, r, dr, d
-            elif state[0] == width: actions are u, ul, l, dl, d
-            elif state[1] == 0: actions are l, dl, d, dr, r
-            elif state[1] == height: actions are l, ul, u, ur, r
-            else: actions are u, ul , l, dl, d, dr, r, ur'''
-        pass
+        """Return the possible directions given the current state."""
+
+        if state == [0,0]:
+            return ['d', 'dr', 'r']
+        elif state == [0,self.height - 1]:
+            return ['u', 'ur', 'r']
+        elif state == [self.width - 1 , 0]:
+            return ['d', 'dl', 'l']
+        elif state == [self.width - 1, self.height - 1]:
+            return ['u', 'ul', 'l']
+        elif state[0] == 0:
+            return ['u', 'ur', 'r', 'dr', 'd']
+        elif state[0] == self.width - 1:
+            return ['u', 'ul', 'l', 'dl', 'd']
+        elif state[1] == 0:
+            return ['l', 'dl', 'd', 'dr', 'r']
+        elif state[1] == self.height - 1:
+            return ['l', 'ul', 'u', 'ur', 'r']
+        else:
+            return ['u', 'ul', 'l', 'dl', 'd', 'dr', 'r', 'ur']
 
     def result(self, state, action):
-        #depending on direction change the value of state to get your output state.
-        pass
+        """depending on direction change the value of state to get your output state."""
+        switch = {'u': [0, -1], 'ul': [-1, -1], 'l': [-1, 0], 'dl': [-1, 1],
+                  'd': [0, 1], 'dr': [1, 1], 'r': [1, 0], 'ur': [1, -1]}
+
+        return[state[0] + switch[action][0], state[1] + switch[action][1]]
+
     def goal_test(self, state):
-        #if state == goal, both are of form [x,y]
-        pass
+        if state == self.goal:
+            return True
+        else:
+            return False
+
 
     def path_cost(self, c, state1, action, state2):
         #using action check against speed from state1 and state2, to calculate the total time
@@ -50,3 +70,8 @@ def main():
     parser.add_argument("-v","--verbose", help="increase output verbosity", action="store_true")
 
     args = parser.parse_args()
+
+
+searcher = Search([1, 1], [2, 2], [[1, 1, 1], [5, 2, 1], [1, 5, 2]])
+print([searcher.result(searcher.initial, i) for i in searcher.actions(searcher.initial)])
+print([searcher.goal_test(i)for i in [searcher.result(searcher.initial, i) for i in searcher.actions(searcher.initial)]])
